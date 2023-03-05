5f4878d71ea2b54045e706c8266a0d66901b6291
physwizz <pollockg86@gmail.com>
2023-03-05 15:40:57 +1100

    first

---
+++ build_kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+make clean && make mrproper
+export CROSS_COMPILE=$(pwd)/toolchain/aarch64-linux-android-4.9-master/bin/aarch64-linux-androidkernel-
+export CC=$(pwd)/toolchain/prebuilts_clang_host_linux-x86_clang-r383902-main/bin/clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export ANDROID_MAJOR_VERSION=r
+
+export KCFLAGS=-w
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physwizz_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
